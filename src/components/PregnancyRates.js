fac39b0c2f5ad3b3dc06613e38ef189874a2aafd
jamesjlandry <james.j.landry@outlook.com>
2021-02-08 15:30:47 -0600

    pregnancy rates component now passes all data accurately to piechart

--- src/components/PregnancyRates.js
+++ src/components/PregnancyRates.js
@@ -7,29 +7,29 @@ const PregnancyRates = (props) => {
 
     let pregKeys = Object.keys(props.pregData)
 
-    // let docs = pregKeys.filter()
-    // let vitTechs = pregKeys.filter()
-    // let thawTechs = pregKeys.filter()
-    // let transTechs = pregKeys.filter()
+    let docs = Object.keys(props.pregData["Trans Doctor"])
+    let vitTechs = Object.keys(props.pregData["Vit tech"])
+    let thawTechs = Object.keys(props.pregData["Thaw tech"])
+    let transTechs = Object.keys(props.pregData["Trans tech"])
     
 
     return(
         <div>
-            {/* <div>
+            <div>
                <PieChart key={"total_pregRate"} tData={props.pregData['Total']}/>
             </div>
             <div>
-                {docs.map( doc => <div> {doc} <PieChart key={`${doc}_pregrate`} tData={props.pregData[doc]} /> </div> )}
+                {docs.map( doc => <div> {doc} <PieChart key={`${doc}_pregrate`} tData={props.pregData["Trans Doctor"][doc]} /> </div> )}
             </div>
             <div>
-                {transTechs.map( tech => <div> {tech} <PieChart key={`${tech}_pregrate`} tData={props.pregData[tech]} /> </div> )}
+                {transTechs.map( tech => <div> {tech} <PieChart key={`${tech}_pregrate`} tData={props.pregData["Trans tech"][tech]} /> </div> )}
             </div>
             <div>
-                {thawTechs.map( tech => <div> {tech} <PieChart key={`${tech}_pregrate`} tData={props.pregData[tech]} /> </div> )}
+                {thawTechs.map( tech => <div> {tech} <PieChart key={`${tech}_pregrate`} tData={props.pregData["Thaw tech"][tech]} /> </div> )}
             </div>
             <div>
-                {vitTechs.map( tech => <div> {tech} <PieChart key={`${tech}_pregrate`} tData={props.pregData[tech]} /> </div> )}
-            </div> */}
+                {vitTechs.map( tech => <div> {tech} <PieChart key={`${tech}_pregrate`} tData={props.pregData["Vit tech"][tech]} /> </div> )}
+            </div>
             
         </div>
     )
