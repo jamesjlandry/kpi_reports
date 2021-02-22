4dbfd685fc77bc650b6e2d10be63e557ab924fca
jamesjlandry <james.j.landry@outlook.com>
2021-02-22 16:54:10 -0600

    started working on login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,41 @@
 import React from 'react'
+import {useState} from 'react'
 
+const [password, setPassword] = useState('')
 
-let Login = () => {
+const handleSubmit = (e) => {
+    e.preventDefault();
+    let pWord = {
+        password: password
+    }
+    login(pWord)
+}
+
+// const login = async (pWord) => {
+//     let response = await fetch(''), {
+       
+//     }
+// }
+
+let LogIn = () => {
     return (
         <div>
-            Login
+            <h1>Welcome Michael</h1>
+            <form onSubmit={e => handleSubmit(e)}>
+                <input 
+                    onChange={e => setPassword(e.target.value)}
+                    id= "password"
+                    name="password"
+                    placeholder="enter password"
+                    type="text"
+                    value={password}
+                    />
+                    <div>
+                        <button type="submit">Log In</button>
+                    </div>
+            </form>
         </div>
     )
 }
 
-export default Login
+export default LogIn
