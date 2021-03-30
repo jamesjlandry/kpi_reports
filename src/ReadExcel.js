b82c82b4176eacd4cd88592f531adefd2fdbe884
jamesjlandry <james.j.landry@outlook.com>
2021-03-29 21:10:43 -0500

    refactored to separate read excel data cleaning

---
+++ src/ReadExcel.js
@@ -0,0 +1,488 @@
+import * as XLSX from 'xlsx';
+
+
+
+
+
+export const ReadExcel = (file) => {
+    return new Promise((resolve, reject) => {
+
+
+        const promise = new Promise((resolve, reject) => {
+            const fileReader = new FileReader()
+            fileReader.readAsArrayBuffer(file)
+
+            fileReader.onload = (e) => {
+                const bufferArray = e.target.result
+
+                const wb = XLSX.read(bufferArray, { type: 'buffer' })
+
+                const sheet1 = wb.SheetNames[0]
+                const sheet2 = wb.SheetNames[1]
+
+                const ws1 = wb.Sheets[sheet1]
+                const ws2 = wb.Sheets[sheet2]
+
+                const data = [
+                    XLSX.utils.sheet_to_json(ws1),
+                    XLSX.utils.sheet_to_json(ws2)
+                ]
+                resolve(data)
+            }
+            // fileReader.onError((error) => {
+            //   reject(error)
+            // })
+        })
+
+        // after the data is set from sheet.js the following logic is specific to the data for the fertility clinic
+        promise.then((data) => {
+
+            const arr1 = data[0]
+            const arr2 = data[1]
+
+            const retRateKeys = []
+            for (let key in arr1) {
+                retRateKeys.push(key)
+            }
+
+            const fetRateKeys = []
+
+            for (let key in arr2) {
+                fetRateKeys.push(key)
+            }
+
+            const ret = []
+            const fet = []
+
+            function copyArray(array, newArr) {
+                for (let key in array) {
+                    newArr.push({})
+                    let row = array[key]
+                    let headers = Object.keys(row)
+                    headers.forEach(header => {
+                        let newKey = header.replace(/[^a-z0-9]/gi, '').toLowerCase()
+                        newArr[key][newKey] = array[key][header]
+                    })
+                }
+            }
+
+            copyArray(arr1, ret)
+
+            copyArray(arr2, fet)
+
+
+
+            const totalICSID = retRateKeys.filter(key => ret[key]['2pn'] !== 'n/a')
+
+
+
+            const fertRateData = {
+                "Total": {
+                    '2PN': 0,
+                    '1PN': 0,
+                    '>2PN': 0,
+                    'Degen': 0,
+                    '0PN': 0
+                }
+            }
+            totalICSID.forEach(key => {
+
+                fertRateData["Total"]['2PN'] += ret[key]['2pn']
+                fertRateData["Total"]['1PN'] += ret[key]['1pn']
+                fertRateData["Total"]['>2PN'] += ret[key]['abnormal']
+                fertRateData["Total"]['Degen'] += ret[key]['deg']
+                fertRateData["Total"]['0PN'] += ret[key]['0pn']
+
+            })
+
+
+            const techICSID = retRateKeys.filter(key => !ret[key]['icsitech'].includes('/'))
+
+            techICSID.forEach(key => {
+
+                if (!fertRateData[ret[key]['icsitech'].toUpperCase()]) {
+
+                    fertRateData[ret[key]['icsitech'].toUpperCase()] = {
+                        '2PN': ret[key]['2pn'],
+                        '1PN': ret[key]['1pn'],
+                        '>2PN': ret[key]['abnormal'],
+                        'Degen': ret[key]['deg'],
+                        '0PN': ret[key]['0pn']
+                    }
+
+                } else {
+                    fertRateData[ret[key]['icsitech'].toUpperCase()]['2PN'] += ret[key]['2pn']
+                    fertRateData[ret[key]['icsitech'].toUpperCase()]['1PN'] += ret[key]['1pn']
+                    fertRateData[ret[key]['icsitech'].toUpperCase()]['>2PN'] += ret[key]['abnormal']
+                    fertRateData[ret[key]['icsitech'].toUpperCase()]['Degen'] += ret[key]['deg']
+                    fertRateData[ret[key]['icsitech'].toUpperCase()]['0PN'] += ret[key]['0pn']
+                }
+
+
+            })
+
+            const cleaveDayThree = retRateKeys.filter(key => ret[key]['day3emb'] !== 'n/a')
+
+
+            const cleaveRateData = {
+                "Total": {
+                    "Good": 0,
+                    "Poor": 0,
+                    "Disc": 0
+                }
+            }
+
+            cleaveDayThree.forEach(key => {
+
+                cleaveRateData["Total"]["Good"] += ret[key]['d36c']
+                cleaveRateData["Total"]["Poor"] += (ret[key]['day3emb'] - ret[key]['d36c'])
+                if (ret[key]["2pn"] > ret[key]['day3emb']) {
+                    cleaveRateData["Total"]["Disc"] += (ret[key]["2pn"] - ret[key]['day3emb'])
+
+                }
+
+            })
+
+
+
+            techICSID.forEach(key => {
+
+                if (!cleaveRateData[ret[key]['icsitech']]) {
+                    if (ret[key]["2pn"] > ret[key]['day3emb']) {
+                        cleaveRateData[ret[key]['icsitech']] = {
+                            "Good": ret[key]['d36c'],
+                            "Poor": (ret[key]['day3emb'] - ret[key]['d36c']),
+                            "Disc": (ret[key]["2pn"] - ret[key]['day3emb']),
+                        }
+                    } else {
+                        cleaveRateData[ret[key]['icsitech']] = {
+                            "Good": ret[key]['d36c'],
+                            "Poor": (ret[key]['day3emb'] - ret[key]['d36c']),
+                            "Disc": 0,
+                        }
+                    }
+
+                } else {
+                    cleaveRateData[ret[key]['icsitech']]['Good'] += ret[key]['d36c']
+                    cleaveRateData[ret[key]['icsitech']]['Poor'] += (ret[key]['day3emb'] - ret[key]['d36c'])
+                    if (ret[key]["2pn"] > ret[key]['day3emb']) {
+                        cleaveRateData[ret[key]['icsitech']]['Disc'] += (ret[key]["2pn"] - ret[key]['day3emb'])
+                    }
+
+                }
+
+
+            })
+
+
+            const kpiData = {
+                'bioRes': {
+                    'Total': 0,
+                },
+                'eupRate': {
+                    "Total": 0,
+                },
+                'embWSurv': {
+                    'Total': 0,
+                },
+                'ooWSurv': {
+                    'Total': 0,
+                },
+                'avgRateEmbTrans': {
+                    'Total': 0,
+                }
+            }
+
+            const bioRateKeys = techICSID.filter(key => ret[key]['noread'] !== 'n/a')
+
+
+
+            const setRate = (array, keys, techIdentifier, numerator, denominator) => {
+                let techs = {
+                    "Total": {
+                        "numerator": 0,
+                        "denominator": 0
+                    },
+                }
+                keys.forEach(key => {
+                    if (!techs[array[key][techIdentifier].toUpperCase()]) {
+                        techs[array[key][techIdentifier].toUpperCase()] = {
+                            "numerator": array[key][numerator],
+                            'denominator': array[key][denominator]
+                        }
+
+                        techs["Total"]["numerator"] += array[key][numerator]
+                        techs["Total"]["denominator"] += array[key][denominator]
+                    } else {
+                        techs[array[key][techIdentifier].toUpperCase()]["numerator"] += array[key][numerator]
+                        techs[array[key][techIdentifier].toUpperCase()]["denominator"] += array[key][denominator]
+                        techs["Total"]["numerator"] += array[key][numerator]
+                        techs["Total"]["denominator"] += array[key][denominator]
+                    }
+                })
+
+                for (let techName in techs) {
+                    let tech = techs[techName]
+                    tech.rate = tech.numerator / tech.denominator
+                }
+                return techs
+            }
+
+            const bioResults = setRate(ret, bioRateKeys, "icsitech", "noread", "totalbx")
+            const blastRateData = setRate(ret, techICSID, "icsitech", "totalusableblast", "2pn")
+
+            const eupRate = setRate(ret, bioRateKeys, "icsitech", "euploid", "totalbx")
+
+            const embWarmSurvRate = setRate(fet, fetRateKeys, "thawtech", "blastsurvived", 'blastthawed')
+
+            // once spreadsheet is updated to include a did not survive row, will be added to Pending add below to add this KPI.
+            // const oOcyteKeys = techICSID.filter( key => ret[key]["Procedure"] === "DE Thaw-GBV")
+            // const ooWSurvRate = setRate(arr1, oOcyteKeys, "icsitech", "Pending add", "procedure" )
+            // kpiData["ooWSurv"] = ooWSurvRate
+
+            for (let key in fet) {
+                let age = fet[key]['agegroup']
+                let blastTrans = fet[key]['blasttrans']
+                if (!kpiData['avgRateEmbTrans'][age]) {
+                    kpiData['avgRateEmbTrans'][age] = {
+                        "denominator": 1,
+                        "numerator": blastTrans
+                    }
+                    kpiData['avgRateEmbTrans']['Total'] = {
+                        "denominator": 1,
+                        "numerator": blastTrans
+                    }
+                } else {
+                    kpiData['avgRateEmbTrans'][age]["denominator"] += 1
+                    kpiData['avgRateEmbTrans'][age]["numerator"] += blastTrans
+                    kpiData['avgRateEmbTrans']['Total']["denominator"] += 1
+                    kpiData['avgRateEmbTrans']['Total']['numerator'] += blastTrans
+                }
+            }
+
+            for (let key in kpiData['avgRateEmbTrans']) {
+                let rate = kpiData['avgRateEmbTrans'][key]['numerator'] / kpiData['avgRateEmbTrans'][key]['denominator']
+                kpiData['avgRateEmbTrans'][key].rate = rate
+            }
+
+
+
+            kpiData['eupRate'] = eupRate
+
+            kpiData['embWSurv'] = embWarmSurvRate
+
+            kpiData['bioRes'] = bioResults
+
+
+
+
+            const fetPregRateData = fetRateKeys.filter(key => fet[key]['agegroup'] === '25-34' || fet[key]['agegroup'] === '35-37')
+
+
+            const pregRateData = {
+                "Total": {
+                    "Pos": 0,
+                    "Neg": 0
+                },
+                "Thaw tech": {},
+                "Vit tech": {},
+                "Trans Doctor": {},
+                "Trans tech": {},
+            }
+
+            fetPregRateData.forEach(key => {
+                fet[key]["posneg"] === "POS" ? pregRateData["Total"]["Pos"] += 1 : pregRateData["Total"]["Neg"] += 1
+            })
+
+
+
+            const setPregRateDataByTech = (tech, keys, hashKey) => {
+                keys.forEach(key => {
+                    if (!pregRateData[hashKey][fet[key][tech].toUpperCase()]) {
+                        if (fet[key]['posneg'] === 'POS') {
+                            pregRateData[hashKey][fet[key][tech].toUpperCase()] = {
+                                "Pos": 1,
+                                "Neg": 0
+                            }
+                        }
+                        else {
+                            pregRateData[hashKey][fet[key][tech].toUpperCase()] = {
+                                "Pos": 0,
+                                "Neg": 1
+                            }
+                        }
+
+                    } else {
+                        fet[key]['posneg'] === 'POS' ? pregRateData[hashKey][fet[key][tech].toUpperCase()]["Pos"] += 1 : pregRateData[hashKey][fet[key][tech].toUpperCase()]["Neg"] += 1
+                    }
+                })
+            }
+
+
+            setPregRateDataByTech("transmd", fetPregRateData, "Trans Doctor")
+            setPregRateDataByTech("transemb", fetPregRateData, "Trans tech")
+            setPregRateDataByTech("vittech", fetPregRateData, "Vit tech")
+            setPregRateDataByTech("thawtech", fetPregRateData, "Thaw tech")
+
+            const pregDataByAge = {
+                "Total": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+                "<35": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+                "35-37": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+                "38-40": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+                "41-42": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+                ">42": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+                "Donor": {
+                    "Neg": 0,
+                    "BC": 0,
+                    "Pos": 0,
+                },
+            }
+
+            const setPregDataByAge = (keys, ageGroup, age) => {
+                keys.forEach(key => {
+
+                    if (fet[key]['agegroup'].toLowerCase() === ageGroup) {
+                        if (fet[key]['posneg'] === "POS") {
+                            if (fet[key]['sacs'] === 0) {
+                                pregDataByAge[age]["BC"]++
+                                pregDataByAge["Total"]["BC"]++
+                            } else {
+                                pregDataByAge[age]["Pos"]++
+                                pregDataByAge["Total"]["Pos"]++
+                            }
+                        } else {
+                            pregDataByAge[age]["Neg"]++
+                            pregDataByAge["Total"]["Neg"]++
+                        }
+                    }
+                })
+            }
+
+
+
+            setPregDataByAge(fetRateKeys, "35-37", "35-37")
+            setPregDataByAge(fetRateKeys, "donor", "Donor")
+            setPregDataByAge(fetRateKeys, "25-34", "<35")
+            setPregDataByAge(fetRateKeys, "41-42", "41-42")
+            setPregDataByAge(fetRateKeys, "38-40", "38-40")
+            setPregDataByAge(fetRateKeys, "43-50", ">42")
+
+
+            const ultraSoundData = {
+                "Total": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+                "<35": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+                "35-37": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+                "38-40": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+                "41-42": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+                ">42": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+                "Donor": {
+                    "HB": 0,
+                    "No HB": 0,
+                    "Mult HB": 0,
+                    "Mult No HB": 0,
+                },
+            }
+
+            const setUltraSoundDataByAge = (keys, ageGroup, age) => {
+                keys.forEach(key => {
+
+                    if (fet[key]['agegroup'].toLowerCase() === ageGroup) {
+                        if (fet[key]['posneg'] === "POS") {
+                            if (fet[key]["blasttrans"] > 1) {
+                                if (fet[key]['hb'] === 0) {
+                                    ultraSoundData[age]["Mult No HB"]++
+                                    ultraSoundData["Total"]["Mult No HB"]++
+                                } else {
+                                    ultraSoundData[age]["Mult HB"]++
+                                    ultraSoundData["Total"]["Mult HB"]++
+                                }
+                            } else {
+                                if (fet[key]['hb'] === 0) {
+                                    ultraSoundData[age]["No HB"]++
+                                    ultraSoundData["Total"]["No HB"]++
+                                } else {
+                                    ultraSoundData[age]["HB"]++
+                                    ultraSoundData["Total"]["HB"]++
+                                }
+                            }
+                        }
+                    }
+                })
+            }
+
+            setUltraSoundDataByAge(fetRateKeys, "35-37", "35-37")
+            setUltraSoundDataByAge(fetRateKeys, "donor", "Donor")
+            setUltraSoundDataByAge(fetRateKeys, "25-34", "<35")
+            setUltraSoundDataByAge(fetRateKeys, "41-42", "41-42")
+            setUltraSoundDataByAge(fetRateKeys, "38-40", "38-40")
+            setUltraSoundDataByAge(fetRateKeys, "43-50", ">42")
+
+            let combinedData = {
+                cleaveRate: cleaveRateData,
+                fertRate: fertRateData,
+                pregRate: pregRateData,
+                pregAgeData: pregDataByAge,
+                ultrasoundData: ultraSoundData,
+                blastRateData: blastRateData,
+                miscKPI: kpiData,
+            }
+           
+            resolve(combinedData)
+
+        })
+    })
+}
